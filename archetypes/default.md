eb7f9bd5b34e8e3af32014771215bfe6a3551da8
MunifTanjim <MunifTanjim@users.noreply.github.com>
2017-05-20 00:37:47 +0600

    add the theme

---
+++ archetypes/default.md
@@ -0,0 +1,4 @@
+---
+categories:
+  - Others
+---
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const webpack = require('webpack')
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+const extractCSS = new ExtractTextPlugin({
+  filename: getPath => getPath('css/[name].css').replace('css', '../css')
+})
+
+module.exports = {
+  entry: {
+    main: path.join(__dirname, 'src/scripts', 'main.js')
+  },
+  output: {
+    path: path.join(__dirname, 'static', 'js'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.join(__dirname, 'src/scripts'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015']
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        include: path.resolve(__dirname, 'src/stylesheets'),
+        loader: extractCSS.extract({
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.scss']
+  },
+  plugins: [
+    extractCSS,
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false
+      }
+    })
+  ]
+}
