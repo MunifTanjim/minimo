3e1fb82978294dd2670fe6fca80b7ea421591b38
MunifTanjim <MunifTanjim@users.noreply.github.com>
2017-06-08 07:00:10 +0600

    add cache invalidation support

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const webpack = require('webpack')
 const path = require('path')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const autoprefixer = require('autoprefixer')
+const AssetsPlugin = require('assets-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-const definePlugin = new webpack.DefinePlugin({
+const define = new webpack.DefinePlugin({
   'process.env': {
     NODE_ENV: JSON.stringify(process.env.NODE_ENV),
     BROWSERSLIST: ['> 1%', 'last 2 versions']
@@ -11,7 +13,27 @@ const definePlugin = new webpack.DefinePlugin({
 })
 
 const extractCSS = new ExtractTextPlugin({
-  filename: getPath => getPath('css/[name].css').replace('css', '../css')
+  filename: getPath =>
+    getPath('css/[name].[contenthash:8].css').replace('css', '../css')
+})
+
+const assetsManifest = new AssetsPlugin({
+  filename: 'assets.json',
+  path: path.join(__dirname, 'data'),
+  fullPath: false,
+  processOutput: assets => {
+    Object.keys(assets).forEach(bundle => {
+      Object.keys(assets[bundle]).forEach(type => {
+        let filename = assets[bundle][type]
+        assets[bundle][type] = filename.slice(filename.indexOf(bundle))
+      })
+    })
+    return JSON.stringify(assets, null, 2)
+  }
+})
+
+const cleanBuild = new CleanWebpackPlugin(['static/css/*', 'static/js/*'], {
+  watch: true
 })
 
 module.exports = {
@@ -19,8 +41,8 @@ module.exports = {
     main: path.join(__dirname, 'src/scripts', 'main.js')
   },
   output: {
-    path: path.join(__dirname, 'static', 'js'),
-    filename: '[name].js'
+    filename: '[name].[chunkhash:8].js',
+    path: path.join(__dirname, 'static', 'js')
   },
   module: {
     rules: [
@@ -60,5 +82,5 @@ module.exports = {
   resolve: {
     extensions: ['*', '.js', '.scss']
   },
-  plugins: [definePlugin, extractCSS]
+  plugins: [cleanBuild, define, extractCSS, assetsManifest]
 }
